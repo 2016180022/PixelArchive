6df5674085db2fd40e815358bc15883762737198
2016180022 <71115355+2016180022@users.noreply.github.com>
2023-07-23 16:07:13 +0900

    플레이어 이동, Raycast Scanning 구현
    1. 플레이어 이동 및 애니메이션을 구현. (temp res 사용)
    2. RayCast 기반의 ObjScanning을 구현. (DebugLog 출력)

---
+++ PixelArchive/Assets/Script/PlayerAction.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    float h;
+    float v;
+    bool isHorizontalMove;
+    Vector3 dirVec;
+    GameObject scanObj;
+    
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // h, v 방향 움직임 설정
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        // 버튼 다운/업 정보 동기화
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //h, v 방향 bool 인자까지 동기화
+        if (hDown) isHorizontalMove = true;
+        else if (vDown) isHorizontalMove = false;
+        else if (hUp || vUp) isHorizontalMove = h != 0;
+
+        if (anim.GetInteger("horizonAxisRaw") != h) {
+            anim.SetBool("isDirectionChanged", true);
+            anim.SetInteger("horizonAxisRaw", (int)h);
+            }
+        else if (anim.GetInteger("vertAxisRaw") != v) {
+            anim.SetBool("isDirectionChanged", true);         
+            anim.SetInteger("vertAxisRaw", (int)v);
+        }
+        else anim.SetBool("isDirectionChanged", false);
+
+        //방향 레이 동기화
+        if (vDown && v == 1) dirVec = Vector3.up;
+        else if (vDown && v == -1) dirVec = Vector3.down;
+        else if (hDown && v == -1) dirVec = Vector3.left;
+        else if (hDown && v == 1) dirVec = Vector3.right;
+
+        //scanObj 검사
+        if (Input.GetButtonDown("Jump") && scanObj != null) Debug.Log("이 녀석은" + scanObj.name + "입니다.");
+    }
+
+    void FixedUpdate()
+    {
+        //h, v 움직임 따로 적용
+        Vector2 moveVector = isHorizontalMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVector * speed;
+
+        //ray 적용
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Obj"));
+
+        if (rayHit.collider != null) scanObj = rayHit.collider.gameObject;
+        else scanObj = null;
+    }
+}
