6c61fae723428e4327c4a91a0081a07c057a0623
2016180022 <71115355+2016180022@users.noreply.github.com>
2023-12-21 00:38:16 +0900

    Cross 타입 타일 추가
    1. Cross 타입의 타일이 출현되게 추가
    2. Room 타입의 타일 생성 시, 마지막 Cross Pivot으로 되돌아가서 재생성하는 로직 추가
    3. CrossPivot 리스트를 잘못 읽는 케이스 확인되어 수정 필요
    4. Cross 타입의 타일 개수를 인자로 조절하는 기능 필요

--- PixelArchive/Assets/Script/TileManager.cs
+++ PixelArchive/Assets/Script/TileManager.cs
@@ -14,14 +14,19 @@ public class TileManager : MonoBehaviour
     public GameObject[] crossTile;
     public int prevTileDir = 8;                                     //직전 생성된 Tile의 방향
     public int minTileCount = 10;                                   //Room이 생성되기 위한 최소 타일 수
+    public int roomCount = 0;
     
     public UnityEngine.Vector2Int tileSize = new Vector2Int(10, 6); //Param으로 받거나 따로 관리해줄 예정이지만 일단 하드코딩
     public UnityEngine.Vector2Int nowPivot = new Vector2Int();
     public List<Vector2Int> pivotList;
     public List<String> randomTileType = new List<string>();
-    public List<int> tileTypeList = new List<int>();           //만들어진 TileType에 대한 List (연속 검사용)    
-    public Dictionary<String, int> tileTypeAndWeight = new Dictionary<string, int>();    //tileType과 가중치
+    public List<int> tileTypeList = new List<int>();           //만들어진 TileType에 대한 List (연속 검사용)
+    public List<Vector2Int> crossedPivotList = new List<Vector2Int>();          //갈림길 Pivot 리스트. 역순으로 검사해서 진행.
+    public List<int> crossedPivotPrevDirList = new List<int>();                 //갈림길 Pivot이 가지는 PrevDir 리스트. 위 리스트와 같이 핸들링.
+    public int crossedPivotCount;                                               //갈림길 Pviot의 count. List.count로 핸들링하기에는 동기화 시점을 맞춰줄 수 없어서 따로 처리.
+    public bool isCorner;                                                       //Corner 타입이 나왔는데, 배치 불가능이라 넘어갔을 경우 다음에도 corner를 넣어줄 처리 인자.
 
+    public Dictionary<String, int> tileTypeAndWeight = new Dictionary<string, int>();    //tileType과 가중치
 
     public void Start() {
         //Hall/Corner 랜덤 뽑기
@@ -38,6 +43,7 @@ public class TileManager : MonoBehaviour
         if (roomTile.Length > 0) tileTypeAndWeight.Add("Room",10);
 
         pivotList.Add(nowPivot);
+        isCorner = false;
 
         //Pivot 기본값 조정
         nowPivot = new Vector2Int(0, 12);
@@ -112,8 +118,13 @@ public class TileManager : MonoBehaviour
         UnityEngine.Vector3 tilePos = new UnityEngine.Vector3(nowPivot.x, nowPivot.y, transform.position.z);
 
         //가중치 랜덤 함수
-        int tileType = getRandom(tileTypeAndWeight);
+        int tileType;
+        if (isCorner == true) tileType = 3;
+        else {tileType = getRandom(tileTypeAndWeight);}
         
+        //테스트용 Corner타일 생성
+        if (pivotList.Count == 10) tileType = 3;
+
         //HallType 중복 검사
         if (tileTypeList.Count >= 2) {
             int length = tileTypeList.Count;
@@ -240,11 +251,97 @@ public class TileManager : MonoBehaviour
             else if (prevTileDir == 6) { GameObject room = Instantiate(roomTile[2], tilePos, transform.rotation); }
             else if (prevTileDir == 8) { GameObject room = Instantiate(roomTile[3], tilePos, transform.rotation); }
 
+            //생성 완료되었으면 카운트 처리
+            roomCount++;
             //Room 생성 후에는 더 이상 해당 Pivot을 트래킹 할 필요가 없으므로, crossedPivotList에서 남은 Pivot을 생성해주러 이동
             Debug.Log("이번 갈림길 Pivot의 Room이 생성되었습니다.");
+            if (crossedPivotList.Count > 1) {
+                //이렇게 처리하려면 처리 완료 시에 역순으로 빼줘야 함
+                nowPivot = crossedPivotList[crossedPivotCount - 1];
+                prevTileDir = crossedPivotPrevDirList[crossedPivotCount - 1];
+                //Pivot을 옮기면 데이터 삭제 처리
+                crossedPivotList.RemoveAt(crossedPivotCount - 1);
+                crossedPivotPrevDirList.RemoveAt(crossedPivotCount - 1);
+                crossedPivotCount--;
+            }
         }
+        else if (tileType == 3) {
+            if (prevTileDir == 2) {
+                //방향 양 쪽 다 만들 수 있는지 먼저 체크
+                if (!checkTile(4, nowPivot) || !checkTile(6, nowPivot)) {
+                    //만약 안되면 생성을 못하니까, 다음 턴에 무조건 corner를 생성하는 로직을 넣어야 함
+                    isCorner = true;
+                    return;
+                }
+                //4, 6 고정이니까 갈림길 하나 만들어주고
+                GameObject corner = Instantiate(crossTile[0], tilePos, transform.rotation);
+                pivotList.Add(nowPivot);
 
+                //이후에 갈 Pivot으로 옮겨줌
+                Vector2Int tempPivot = nowPivot;
+                tempPivot.x -= tileSize.x * 2;
+                crossedPivotList.Add(tempPivot);
+
+                //6 방향으로 진행하도록 PrevDir 6 넣어주고 처리도 해줌
+                // pivotList.Add(nowPivot);
+                nowPivot.x += tileSize.x * 2;
+                prevTileDir = 6;
+                //나머지 4는 List에 저장
+                crossedPivotPrevDirList.Add(4);
+            }
+            else if (prevTileDir == 8) {
+                if (!checkTile(4, nowPivot) || !checkTile(6, nowPivot)) {
+                    isCorner = true;
+                    return;
+                }
+                GameObject corner = Instantiate(crossTile[3], tilePos, transform.rotation);
+                pivotList.Add(nowPivot);
+
+                Vector2Int tempPivot = nowPivot;
+                tempPivot.x -= tileSize.x * 2;
+                crossedPivotList.Add(tempPivot);
+
+                nowPivot.x += tileSize.x * 2;
+                prevTileDir = 6;
+                crossedPivotPrevDirList.Add(4);
+            }
+            else if (prevTileDir == 6) {
+                if (!checkTile(2, nowPivot) || !checkTile(8, nowPivot)) {
+                    isCorner = true;
+                    return;
+                }
+                GameObject corner = Instantiate(crossTile[2], tilePos, transform.rotation);
+                pivotList.Add(nowPivot);
+
+                Vector2Int tempPivot = nowPivot;
+                tempPivot.y -= tileSize.y * 2;
+                crossedPivotList.Add(tempPivot);
+
+                nowPivot.y += tileSize.y * 2;
+                prevTileDir = 8;
+                crossedPivotPrevDirList.Add(2);
+            }
+            else if (prevTileDir == 4) {
+                if (!checkTile(2, nowPivot) || !checkTile(8, nowPivot)) {
+                    isCorner = true;
+                    return;
+                }
+                GameObject corner = Instantiate(crossTile[1], tilePos, transform.rotation);
+                pivotList.Add(nowPivot);
+
+                Vector2Int tempPivot = nowPivot;
+                tempPivot.y -= tileSize.y * 2;
+                crossedPivotList.Add(tempPivot);
+
+                nowPivot.y += tileSize.y * 2;
+                prevTileDir = 8;
+                crossedPivotPrevDirList.Add(2);
+            }
+            crossedPivotCount++;
+        }
+        
         tileTypeList.Add(tileType); //정상적으로 추가 되었을 때만 tileTypeList에 추가
+        //crossedPivot이 잘못 저장되는 케이스가 있는 것 같다.
 
         Debug.Log("nowPivot: (" + nowPivot.x + ", " + nowPivot.y + ")");
         Debug.Log("nowTileType:" + tileType);
